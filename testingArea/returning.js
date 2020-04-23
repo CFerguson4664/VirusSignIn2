b151491ba3b866cf0703bdc4be4d4079a2f4fdd7
ewannemacher13 <ewannemacher13@gmail.com>
2020-04-23 17:33:45 -0400

    Security Panel initial testing

---
+++ testingArea/returning.js
@@ -0,0 +1,109 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").className = "sidenav-open"
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").className = "sidenav-closed"
+}
+
+//Called when the user clicks a name button
+function predictButton(value) {
+    document.getElementById('nameText').value = value.innerHTML;
+    document.getElementById('nameText').name = value.id;
+    document.getElementById("submit-event").className = "ready";
+    document.getElementById("subFoot").className = "bg-dark-float-on";
+    closeNav();
+}
+
+//Called when the backspace button is pressed. Handles seaching when backspace key is pressed because
+//pressing backspace doesn't call the keypress event the other handler is looking for
+$(document).ready(function () {
+
+    //Called when text box is selected. Checks to see if there is a ',' in the box.
+    //Comma would come from clicking a name button. To ensure search works properly all text after and including the ','
+    //is removed. Then gets a list of names from the server and populates the choices
+    $('#nameText').click(function(event) {
+        var name = $(this).val()
+        if(name.includes(',')) {
+            document.getElementById('nameText').value = name.substring(0,name.indexOf(','));
+        }
+
+        var data = document.getElementById('nameText').value;
+        document.getElementById("submit-event").className = "not-ready";
+        document.getElementById("subFoot").className = "bg-dark-float-off";
+
+        $.ajax({
+            global: false,
+            type: 'POST',
+            url: '/names',
+            dataType: 'html',
+            data: {
+                name: data
+            },
+            success: function (result) {
+                document.getElementById('mySidenav').innerHTML = result
+            },
+            error: function (request, status, error) {
+                serviceError();
+            }
+        });
+
+        
+    });
+
+    //Called when a new character is typed in the name textbox, gets list of names from server and populates the choices
+    $('#nameText').on('input',function(event) {
+
+        document.getElementById("submit-event").className = "not-ready";
+        document.getElementById("subFoot").className = "bg-dark-float-off";
+    
+        $.ajax({
+            global: false,
+            type: 'POST',
+            url: '/names',
+            dataType: 'html',
+            data: {
+                name: document.getElementById('nameText').value
+            },
+            success: function (result) {
+                document.getElementById('mySidenav').innerHTML = result
+            },
+            error: function (request, status, error) {
+                serviceError();
+            }
+        });
+        
+    });
+
+    //Called when the user clicks the continue button, submits the data to the server
+    $('#submit-event').click(function(event) { 
+
+        $.ajax({
+            global: false,
+            type: 'POST',
+            url: '/returningdata', //The url to post to on the server
+            dataType: 'html',
+
+            //The data to send to the server
+            data: { 
+                userId : document.getElementById('nameText').name
+            },
+
+            //The response from the server
+            success: function (result) { 
+                window.location.replace(result)
+            },
+
+            //Handle any errors
+            error: function (request, status, error) { 
+                serviceError();
+            }
+        });
+    });
+
+    $('#back').click(function(event) {
+        window.location.replace('/usertype');
+    })
+});
---
+++ testingArea/security.js
@@ -0,0 +1,119 @@
+function isIdentificationTypeNNumber(identification) {
+    var regEx = new RegExp('[0-9]');
+    return regEx.test(identification);
+}
+
+function checkEmail(value) {
+    //Taken from emailregex.com
+    var regEx = new RegExp('^.+@.+\\..+$');
+
+    return regEx.test(value);
+}
+
+// uses simple regex to determine whether entered nnumber is valid
+// probably not needed
+function checkStudent() {
+    // var checkRadio = document.getElementById('selected').getAttribute('data-choiceId');
+
+    value = document.getElementById('identification').value;
+    var regEx = new RegExp('^[N,n][0-9]{8}$');
+
+    return regEx.test(value);
+}
+
+function checkall() {
+    // I need comments!
+    var good = (document.getElementById('identification').value != '');
+    // good = good && checkEmail(email);
+    // good = good && checkStudent();
+    
+    if(good) {
+        document.getElementById("submit-event").className = "ready";
+        document.getElementById("subFoot").className = "bg-dark-float-on";
+    }
+    else {
+        document.getElementById("submit-event").className = "not-ready";
+        document.getElementById("subFoot").className = "bg-dark-float-off";
+    }
+}
+
+
+
+function button_click(sender)
+{
+    var buttons = document.getElementsByName("student");
+
+    for (var i = 0; i < buttons.length; i++)
+    {
+        buttons[i].className = "";
+        buttons[i].id = "";
+
+        if(buttons[i] == sender){
+
+            sender.className = "selected";
+        }
+    }
+
+    sender.id = "selected";
+    checkall();
+}
+
+//AJAX Functions
+
+//Wait to execute until AJAX is ready
+$(document).ready(function ()  {
+    
+    // here for record keeping
+    // $('#lastname').on('input',function(event) {
+    //     checkall($('#email').val());
+    // });
+
+    $('#identification').on('input',function(event) {
+        checkStudent(); 
+    });
+
+    //Called when user clicks 'Submit' button
+    $('#submit-event').click(function(event) { 
+        
+        
+        var visitorIdentification = document.getElementById('identification').value;
+        var isEntryAllowed = document.getElementById('selected').getAttribute('data-choiceId');
+
+        var typeOfIdentification = 'name';
+        if (isIdentificationTypeNNumber(visitorIdentification)) {
+            typeOfIdentification = 'nnumber';
+        }
+
+        console.log(visitorIdentification);
+        console.log(typeOfIdentification);
+        console.log(isEntryAllowed);
+        document.getElementById('identification').value = '';
+        
+        $.ajax({
+
+            global: false,
+            type: 'POST',
+            url: '/new/data', //The url to post to on the server
+            dataType: 'html',
+
+            //The data to send to the server
+            data: { 
+                id : visitorIdentification,
+                idType : typeOfIdentification,
+                allowed : isEntryAllowed
+            },
+
+            //The response from the server
+            success: function (result) { 
+                // window.location.replace(result);
+                document.getElementById('submit').className = 'selected';
+            },
+
+            //Handle any errors
+            error: function (request, status, error) { 
+                document.getElementById('submit').className = 'selected';
+                serviceError();
+            }
+        });
+    });
+});
