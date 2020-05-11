f1f79229a25c687ec85844a7d6ba6ec6ee95ffa4
ewannemacher13 <ewannemacher13@gmail.com>
2020-05-11 11:35:52 -0400

    Security Panel Work

--- Node/Pages/S-Security.js
+++ Node/Pages/S-Security.js
@@ -1,18 +1,48 @@
 //**************************************************** IMPORTS **************************************************
 
+//Requires Express Node.js framework
+const express = require('express');
 
 //***************************************************** SETUP ***************************************************
 
+//router to handle moving the get/post requests around
+var router = express.Router();
+
+//Export the router so that Main can access it and our GET/POST functions
+module.exports = router;
+
 //********************************************* GET / POST Requests *********************************************
 
+//Handles the get request for the starting form of this page
+router.get('/',function(req,res) {
+    //Get the starting form of the webpage
+    getPage(function(HTML) {
+        //Send the HTML to the client
+        res.write(HTML);
+        //End our response to the client
+        res.end();
+    });
+});
 
 //********************************************** DEFAULT FUNCTIONS **********************************************
 
 function getPage(callback) {
-    callback(Template());
+    callback(Template(`<div class="button-like">
+            <h2 class="label text-center">Visitor identification:</h2>
+            <input type="text" name="identification" id="identification" data-userId="0" autocomplete="off" class="text2" maxlength="50" disabled="true">
+            
+        </div>
+        <div class="button-like">
+            <h2 class="label text-center">Visitor allowed entry?</h2>
+            <div class="sidenav-open">
+                <button name="student" onclick="button_click(this)" data-choiceId="1">Yes</button>
+                <button name="student" onclick="button_click(this)" data-choiceId="0" id='' class="">No</button>
+            </div>
+            <button id="submit-event" class="not-ready">Submit</button>
+        </div>`));
 }
 
-function Template() {
+function Template(userHTML) {
     var html = `<!DOCTYPE html>
     <html>
         <head>
@@ -20,7 +50,7 @@ function Template() {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>NSCC Sign In</title>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-            <script src="new.js"></script>
+            <script src="security.js"></script>
         </head>
         <header class="bg-dark">
             <div class="logo">
@@ -28,38 +58,15 @@ function Template() {
             </div>
         </header>
         <header class="bg-dark-header">
-            <button id="back" class="ready">Previous page</button>
         </header>
+        
         <main class="bg-light">
-
-            <div class="button-like">
-                <h2 class="label text-center">Enter your first name</h2>
-                <input type="text" name="firstname" id="firstname" autocomplete="off" class="text2" maxlength="50">
-            </div>
-            <div class="button-like">
-                <h2 class="label text-center">Enter your last name</h2>
-                <input type="text" name="lastname" id="lastname" autocomplete="off" class="text2" maxlength="50">
-            </div>
-            <div class="button-like">
-                <h2 class="label text-center">Enter your email</h2>
-                <input type="text" name="email" id="email" autocomplete="off" class="text2" maxlength="75">
-                <div id='emailerror'></div>
-            </div>
-            <div class="button-like">
-                <h2 class="label text-center">Are you a student at Northwest?</h2>
-                <div class="sidenav-open">
-                    <button name="student" onclick="button_click(this)" data-choiceId="1">Yes</button>
-                    <button name="student" onclick="button_click(this)" data-choiceId="0" id='selected' class="selected">No</button>
-                </div>
-            </div>
-            <div class="button-like" id="nndiv" style="display:none;">
-                <h2 class="label text-center">Enter your N-number</h2>
-                <input type="text" name="nnumber" id="nnumber" autocomplete="off" class="text2" value='N' maxlength="10">
-                <div id='nnerror'></div>
-            </div>
+            ${userHTML}
         </main>
+        <input type="text" id="nNumber">
+
         <footer class="bg-dark-float-off" id="subFoot">
-            <button id="submit-event" class="not-ready">Submit</button>
+            
         </footer>
         <footer class="bg-dark">
             <div id="social-icons">
